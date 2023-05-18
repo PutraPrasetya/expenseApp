56ad1d32e68c92c146af651a5b55ea191abc42b2
Komang <putra180401@gmail.com>
2023-05-19 07:58:54 +0800

    Create Multiple Components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
   const expense = [
@@ -6,48 +6,35 @@ function App() {
       id: 1,
       title: "Car Insurances",
       amount: 256.65,
-      date: new Date(2023, 5, 11),
+      date: new Date(2023, 4, 15),
     },
     {
       id: 2,
       title: "Biznet",
       amount: 300,
-      date: new Date(2023, 6, 11),
+      date: new Date(2023, 6, 8),
     },
     {
       id: 3,
       title: "Switch Online",
       amount: 100.2,
-      date: new Date(2023, 7, 11),
+      date: new Date(2023, 7, 4),
     },
     {
       id: 4,
       title: "Youtube",
       amount: 50.5,
-      date: new Date(2023, 8, 11),
+      date: new Date(2023, 8, 12),
     },
   ];
 
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expense[0].title}
-        amount={expense[0].amount}
-        date={expense[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expense[1].title}
-        amount={expense[1].amount}
-        date={expense[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expense[1].title}
-        amount={expense[1].amount}
-        date={expense[1].date}
-      ></ExpenseItem>
+      <Expenses expense={expense} />
     </div>
   );
 }
 
 export default App;
+
