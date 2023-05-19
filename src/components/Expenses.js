e616704ad74cddfe08f0c3324f9b60b4bee488db
Komang <putra180401@gmail.com>
2023-05-19 08:15:24 +0800

    Composition (children Prop)

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,31 +1,32 @@
+import Card from './Card';
 import ExpenseItem from './ExpenseItem'
 import './Expenses.css'
 function Expenses(props) {
-    const expense = props.expense;
+    const items = props.expense;
 
   return (
-    <div className='expenses'>
+    <Card className='expenses'>
       <ExpenseItem
-        title={expense[0].title}
-        amount={expense[0].amount}
-        date={expense[0].date}
+        title={items[0].title}
+        amount={items[0].amount}
+        date={items[0].date}
       ></ExpenseItem>
       <ExpenseItem
-        title={expense[1].title}
-        amount={expense[1].amount}
-        date={expense[1].date}
+        title={items[1].title}
+        amount={items[1].amount}
+        date={items[1].date}
       ></ExpenseItem>
       <ExpenseItem
-        title={expense[2].title}
-        amount={expense[2].amount}
-        date={expense[2].date}
+        title={items[2].title}
+        amount={items[2].amount}
+        date={items[2].date}
       ></ExpenseItem>
       <ExpenseItem
-        title={expense[3].title}
-        amount={expense[3].amount}
-        date={expense[3].date}
+        title={items[3].title}
+        amount={items[3].amount}
+        date={items[3].date}
       ></ExpenseItem>
-    </div>
+    </Card>
   );
 }
 
