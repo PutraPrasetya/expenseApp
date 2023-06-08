4a737ece4198f2785e547e24aa11627d999a1ac2
Komang <putra180401@gmail.com>
2023-06-09 02:05:49 +0800

    Rendering List & Conditional Contents

--- src/App.js
+++ src/App.js
@@ -1,36 +1,42 @@
+import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
+
+const DUMMY_EXPENSES = [
+  {
+    id: 1,
+    title: "Car Insurances",
+    amount: 256.65,
+    date: new Date(2021, 4, 15),
+  },
+  {
+    id: 2,
+    title: "Biznet",
+    amount: 300,
+    date: new Date(2021, 6, 8),
+  },
+  {
+    id: 3,
+    title: "Switch Online",
+    amount: 100.2,
+    date: new Date(2020, 7, 4),
+  },
+  {
+    id: 4,
+    title: "Youtube",
+    amount: 50.5,
+    date: new Date(2019, 8, 12),
+  },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: 1,
-      title: "Car Insurances",
-      amount: 256.65,
-      date: new Date(2022, 4, 15),
-    },
-    {
-      id: 2,
-      title: "Biznet",
-      amount: 300,
-      date: new Date(2021, 6, 8),
-    },
-    {
-      id: 3,
-      title: "Switch Online",
-      amount: 100.2,
-      date: new Date(2020, 7, 4),
-    },
-    {
-      id: 4,
-      title: "Youtube",
-      amount: 50.5,
-      date: new Date(2019, 8, 12),
-    },
-  ];
+  //expenses memiliki nilai yg ada di dummy_expenses
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = expense => {
-    console.log('In App.js');
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    })
   };
 
   return (
---
+++ src/components/Expenses/ExpensesList.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./ExpensesList.css";
+import ExpenseItem from "./ExpenseItem";
+//CEK OPSI IF CONDITION DI SECTION 5
+const ExpensesList = (props) => {
+  if (props.items.length === 0) {
+    return <h2 className="expenses-list__fallback">No Expenses Found.</h2>
+  }
+
+  return (
+    <ul className="expenses-list">
+      {props.items.map((expense) => (
+        <ExpenseItem
+          key={expense.id} //key penting buat dimasukin
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default ExpensesList;
